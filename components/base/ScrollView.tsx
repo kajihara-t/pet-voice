ed37e84f4ef9f8b514d1aecc2d7f2be56b5dd2e4
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-12-24 02:53:01 +0900

    docs: generate jsdoc

--- components/base/ScrollView.tsx
+++ components/base/ScrollView.tsx
@@ -2,6 +2,29 @@ import React from "react";
 import { ScrollView as RNScrollView, StyleSheet } from "react-native";
 import { BaseScrollViewProps } from "../types/Base";
 
+/**
+ * A basic ScrollView component that wraps its children in a RNScrollView component.
+ *
+ * Has the same API as RNScrollView.
+ *
+ * @prop {React.ReactNode} children - The content to be scrolled.
+ * @prop {ViewStyle} style - Additional styles for the RNScrollView.
+ * @prop {boolean} showsVerticalScrollIndicator - Whether to show the vertical scroll indicator.
+ * @prop {boolean} showsHorizontalScrollIndicator - Whether to show the horizontal scroll indicator.
+ *
+ * @example
+ * <ScrollView>
+ *   <Text>Scrollable content</Text>
+ * </ScrollView>
+ *
+ * @example
+ * <ScrollView
+ *   showsVerticalScrollIndicator={false}
+ *   showsHorizontalScrollIndicator={false}
+ * >
+ *   <Text>Scrollable content with no scroll indicators</Text>
+ * </ScrollView>
+ */
 export const ScrollView: React.FC<BaseScrollViewProps> = ({
   children,
   style,
