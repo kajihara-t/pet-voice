03792cd6d5fb16536d62e3b907ddf215e98d6f0d
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-12-26 01:58:58 +0900

    feat: customized center tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,6 +6,15 @@ import { Pressable, View, StyleSheet } from "react-native";
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
 import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+
+import Animated, {
+  useAnimatedStyle,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -13,6 +22,53 @@ function TabBarIcon(props: {
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+function TabBarCenterButton({ focused }: { focused: boolean }) {
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: withRepeat(
+          withSequence(
+            withTiming(1.1, { duration: 1200 }),
+            withTiming(1, { duration: 1200 })
+          ),
+          -1
+        ),
+      },
+    ],
+    shadowColor: "#E1306C",
+    shadowOpacity: withRepeat(
+      withSequence(
+        withTiming(0.6, { duration: 1000 }),
+        withTiming(0.3, { duration: 1000 })
+      ),
+      -1
+    ),
+    zIndex: 1000,
+  }));
+
+  return (
+    <View style={styles.centerButton}>
+      <Animated.View style={[styles.buttonWrapper, animatedStyle]}>
+        <LinearGradient
+          colors={[
+            "#405DE6",
+            "#5B51D8",
+            "#833AB4",
+            "#C13584",
+            "#E1306C",
+            "#FD1D1D",
+          ]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+          style={styles.gradient}
+        >
+          <MaterialCommunityIcons name="paw" size={32} color="#FFFFFF" />
+        </LinearGradient>
+      </Animated.View>
+    </View>
+  );
 }
 
 export default function TabLayout() {
@@ -53,16 +109,7 @@ export default function TabLayout() {
         name="(peek)"
         options={{
           title: "peek",
-          tabBarIcon: ({ focused }) => (
-            <View style={styles.centerButton}>
-              <View style={[styles.button, { backgroundColor: tintColor }]}>
-                <TabBarIcon
-                  name="image"
-                  color={focused ? "#FFFFFF" : "#FFFFFF80"}
-                />
-              </View>
-            </View>
-          ),
+          tabBarIcon: ({ focused }) => <TabBarCenterButton focused={focused} />,
           headerShown: false,
         }}
       />
@@ -84,6 +131,8 @@ const styles = StyleSheet.create({
     top: -20,
     justifyContent: "center",
     alignItems: "center",
+    position: "relative",
+    zIndex: 100,
   },
   button: {
     width: 60,
@@ -94,10 +143,28 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 4,
     },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
     elevation: 5,
+  },
+  buttonWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  gradient: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
